3a6bae2299d3d268921d316043c4289cffad349d
pratyushashukla <pratyusha.shukla212@gmail.com>
2024-04-20 22:00:06 -0400

    added gitignore file

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,8 @@ WORKDIR /app
 COPY requirements.txt .
 COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
-#COPY app1.py drowsiness_detection.py Ear_calculator.py index.py ./
-#COPY drowsiness_detection/dataset ./drowsiness_detection/dataset
-#COPY drowsiness_detection/sound\ files ./drowsiness_detection/sound\ files
-#COPY drowsiness_detection/static ./drowsiness_detection/stat
-#COPY Drowsiness_Detection/templates ./drowsiness_detection/templas
-#COPY License op_webcam.csv Readme.md ./
 EXPOSE 5000
 ENV FLASK_APP=app1.py
+ENV DISPLAY=:0
 CMD ["flask", "run", "--host=0.0.0.0"]
 
--- app1.py
+++ app1.py
@@ -11,7 +11,7 @@ app.config['FLASK_ENV'] = 'development'
 app.config['SECRET_KEY'] = secret_key
 app.config['DEBUG'] = True
 
-# Defining the home page of our site
+
 @app.route("/",methods=['GET', 'POST'])
 def home():
     print(request.method)
@@ -20,7 +20,6 @@ def home():
         if request.form.get('Continue') == 'Continue':
            return render_template("test1.html")
     else:
-        # pass # unknown
         return render_template("index.html")
 
 @app.route("/start", methods=['GET', 'POST'])
@@ -28,23 +27,16 @@ def index():
     print(request.method)
     if request.method == 'POST':
         if request.form.get('Start') == 'Start':
-            # pass
             d_dtcn()
             return render_template("index.html")
     else:
-        # pass # unknown
         return render_template("index.html")
 
 @app.route('/contact', methods=['GET', 'POST'])
 def cool_form():
     if request.method == 'POST':
-        # do stuff when the form is submitted
-
-        # redirect to end the POST handling
-        # the redirect can be to the same route or somewhere else
         return redirect(url_for('index'))
 
-    # show the form, it wasn't submitted
     return render_template('contact.html')
 
 if __name__ == "__main__":
